6c593323acacd5e1caaa8a26c1d33b6d14cd027c
Joel Lobo <joellobo@gmail.com>
2019-09-18 20:22:52 -0300

    add help

--- microprofile-start-cli.sh
+++ microprofile-start-cli.sh
@@ -6,15 +6,40 @@ PROTOCOL="https://"
 URL_BASE="start.microprofile.io/"
 CONTEXT="api/"
 API_VERSION="1/"
-RESOURCE="mpVersion"
+RESOURCE_VERSION="mpVersion"
 
+RESOURCE_PROJECT=""
+
+NAME_ZIP=""
+SERVER=""
+SPEC=""
+GROUP_ID=""
+ARTIFACT_ID=""
 
 if [ "$1" == 'init' ]; then
-    echo "init"
+    curl -O -J https://start.microprofile.io/api/1/project?supportedServer=TOMEE
 fi
 
 if [ "$1" == "help" ]; then
+    echo "---------------------------------------------------------------------------------------"
+    echo " "
     echo "Microprofile Start CLI - HELP "
+    echo " "
+    echo "--groupId=<GROUP_ID>"
+    echo "Ex: org.example"
+    echo " "
+    echo "--artifactId=<ARTIFACT_ID>"
+    echo "Ex: myapp"
+    echo " "
+    echo "--server=<SERVER>"
+    echo "KUMULUZEE,LIBERTY,PAYARA_MICRO,TOMEE"
+    echo " "
+    echo "--spec=<SPEC>"
+    echo "CONFIG,FAULT_TOLERANCE,JWT_AUTH,METRICS,HEALTH_CHECKS,OPEN_API,OPEN_TRACING,REST_CLIENT"
+    echo ""
+    echo "--name=<NAME>"
+    echo " "
+    echo "---------------------------------------------------------------------------------------"
 fi
 
 if [ "$1" == "version" ]; then
